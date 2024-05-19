b4973fe3d0bbe5cf2d1b5354f2a9df9bd761ba3f
Jose Sandoval <Software.Sandoval@gmail.com>
2024-05-19 01:22:28 -0500

    merge main and updates before push

--- frontend-cosmic-chatter/src/app/chatter/page.tsx
+++ frontend-cosmic-chatter/src/app/chatter/page.tsx
@@ -3,7 +3,17 @@ import React from "react";
 const page = () => {
   return (
     <>
-      <div style={{ backgroundImage: `url('../../images/forum.png')`, height: '100vh', backgroundSize: 'cover', backgroundPosition: 'center' }} className='flex justify-center w-full mt-32'>Chatter</div>
+      <div
+        style={{
+          backgroundImage: `url('../../images/forum.png')`,
+          height: "100vh",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className='flex justify-center w-full '
+      >
+        <div className='flex mt-32'>Chatter</div>
+      </div>
     </>
   );
 };
--- frontend-cosmic-chatter/src/app/profile/page.tsx
+++ frontend-cosmic-chatter/src/app/profile/page.tsx
@@ -18,7 +18,7 @@ const page = () => {
             </h4>
             <div className='flex justify-start my-3'>
               <img
-                className='w-full m-3  h-12 w-12 rounded-full ring-2 ring-white'
+                className='w-full m-3  h-10 w-10 rounded-full ring-2 ring-white'
                 src='https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
                 alt='avatar image'
               ></img>
