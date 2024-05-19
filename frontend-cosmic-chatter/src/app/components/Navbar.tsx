b0a0c82ab761482936c54d09533235f78ed02289
trevor-lowder <lowder.trevor@yahoo.com>
2024-05-18 17:23:15 -0700

    update page routing, navbar

---
+++ frontend-cosmic-chatter/src/app/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Link from "next/link";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+const Navbar = () => {
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Cosmic Chatter
+          </Typography>
+          <Box sx={{ display: "flex" }}>
+            <Link href={"/work"} style={{ marginRight: 10 }}>
+              Work
+            </Link>
+            <Link href={"/chatter"} style={{ marginRight: 10 }}>
+              Chatter
+            </Link>
+            <Link href={"/profile"} style={{ marginRight: 10 }}>
+              Profile
+            </Link>
+            <Link href={"/contact"} style={{ marginRight: 2 }}>
+              Contact
+            </Link>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ frontend-cosmic-chatter/src/app/work/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Work</div>;
+};
+
+export default page;
