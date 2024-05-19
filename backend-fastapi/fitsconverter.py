d87d76a24cdb3d7bd0dba9720bcccafdaead0a06
kenStephens <kendall.j.stephens@gmail.com>
2024-05-18 18:04:28 -0700

    updated correctly

--- backend-fastapi/fitsconverter.py
+++ backend-fastapi/fitsconverter.py
@@ -1,7 +1,7 @@
 from astropy.io import fits
 import numpy as np
 
-filename = "backend-fastapi/asset/SDSS_J101547.86+140606.5_1.fits"  # Replace with your actual filename
+filename = "backend-fastapi/asset/SDSS_J101547.86+140606.5_1.fits"  # Filename from assest folder
 hdulist = fits.open(filename)
 data = hdulist[1].data  # Assuming data is in the second extension (modify as needed)
 
@@ -12,7 +12,7 @@ data_array = np.asarray(data)
 delimiter = ","
 
 # Save the data as a CSV file
-np.savetxt("your_data.csv", data_array, delimiter=delimiter)
+np.savetxt("star_file.csv", data_array, delimiter=delimiter)
 
 # Close the FITS file
 hdulist.close()
