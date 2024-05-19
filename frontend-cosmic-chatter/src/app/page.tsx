d2c94acbda292f1fe1c0b1a4ac8ab0f3d8ee7e65
helloworldmeowser <105734247+helloworldmeowser@users.noreply.github.com>
2024-05-18 23:13:56 -0700

    homepage input

--- frontend-cosmic-chatter/src/app/page.tsx
+++ frontend-cosmic-chatter/src/app/page.tsx
@@ -5,7 +5,18 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main} style={{ backgroundImage: `url('/images/home-background.png')`, height: '100vh', backgroundSize: 'cover', backgroundPosition: 'center' }}>
-      <div>Home</div>
+      <div className={styles.header}>
+        <h1 className={styles.title}>COSMIC</h1>
+        <h2 className={styles.subtitle}>CHATTER</h2>
+      </div>
+      <div className={styles.description}>
+        Welcome, Cosmic Detective! Tune into the whispers of the universe. With every signal you analyze, you step closer to uncovering the secrets of distant galaxies and potentially discovering signs of extraterrestrial intelligence and technological activity.
+      </div>
+      <nav className={styles.nav}>
+        <a href="#">Record Audio</a>
+        <a href="#">Upload Audio</a>
+        <a href="#">Analyze Audio</a>
+      </nav>
 
     </main>
   );
